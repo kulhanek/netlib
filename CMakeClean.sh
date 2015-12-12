27e7281c87ea0c8759c9bde818a1f2e87376edc8
Petr Kulhanek <kulhanek@chemi.muni.cz>
2015-12-12 13:49:45 +0100

    unx/win build

--- CMakeClean.sh
+++ CMakeClean.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-if [ $# -gt 0 ]
-	then
-		rm -rf `find . -name CMakeFiles`
-		rm -rf `find . -name CMakeCache.txt`
-		rm -rf `find . -name cmake_install.cmake`
-		rm -rf `find . -name Makefile`
-		rm -rf `find . -name '*~'`
-		rm -rf `find . -name '*.o'`
-		rm -rf `find . -name '*.a'`
-		rm -rf `find . -name '*.so*'`
-		rm -rf `find . -name '*.mod'`
-		rm -rf bin/*
-	else
-		rm -rf `find . -name CMakeFiles`
-		rm -rf `find . -name CMakeCache.txt`
-		rm -rf `find . -name cmake_install.cmake`
-		rm -rf `find . -name Makefile`
-		rm -rf `find . -name '*~'`
-		rm -rf `find . -name '*.o'`
-	fi
+MODE=$1
+if [ -z "$MODE" ]; then
+    MODE="quick"
+fi
+
+case $MODE in
+    "quick")
+        rm -rf `find . -name CMakeFiles`
+        rm -rf `find . -name CMakeCache.txt`
+        rm -rf `find . -name cmake_install.cmake`
+        rm -rf `find . -name Makefile`
+        rm -rf `find . -name '*~'`
+        rm -rf `find . -name '*.o'`
+    ;;
+    "deep")
+        rm -rf `find . -name CMakeFiles`
+        rm -rf `find . -name CMakeCache.txt`
+        rm -rf `find . -name cmake_install.cmake`
+        rm -rf `find . -name Makefile`
+        rm -rf `find . -name '*~'`
+        rm -rf `find . -name '*.o'`
+        rm -rf `find . -name '*.a'`
+        rm -rf `find . -name '*.so'`
+        rm -rf `find . -name '*.so.*'`
+        rm -rf `find . -name '*.mod'`
+        rm -rf bin/*
+    ;;
+esac
+
